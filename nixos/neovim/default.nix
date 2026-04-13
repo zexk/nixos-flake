827b3e99b11f57fbc7cc152a5a351481204d27c2
Bouraoui Ochi <bouraoui@outlook.it>
2026-04-13 22:36:14 +0200

    feat: Add Neovim configuration and plugins setup

---
+++ nixos/neovim/default.nix
@@ -0,0 +1,27 @@
+{ inputs, pkgs, ... }:
+{
+  programs.neovim = {
+    enable = true;
+    package = inputs.neovim-nightly.packages.${pkgs.system}.default;
+    defaultEditor = true;
+    viAlias = true;
+    configure = {
+      customLuaRC = builtins.readFile ./init.lua;
+      packages.myVimPackage = with pkgs.vimPlugins; {
+        start = [
+					mini-diff
+					mini-git
+					mini-statusline
+          blink-cmp
+          blink-pairs
+          kanagawa-nvim
+          mini-pick
+          nvim-lspconfig
+          oil-nvim
+          snacks-nvim
+          stay-centered-nvim
+        ];
+      };
+    };
+  };
+}
