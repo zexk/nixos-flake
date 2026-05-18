d69eb883c69c2da984204b534dc05df1abe498cf
Bouraoui Ochi <bouraoui@outlook.it>
2026-05-18 20:05:38 +0200

    caddy: add caddy + cloudflared reverse proxy module
    - Serve /srv/http on port 80 with gzip/zstd encoding
    - Cloudflare tunnel for zexk.xyz and www.zexk.xyz
    - Open firewall ports 80 and 443

---
+++ modules/caddy.nix
@@ -0,0 +1,33 @@
+{
+  flake.nixosModules.caddy =
+    { inputs, ... }:
+    {
+      services.caddy = {
+        enable = true;
+        virtualHosts.":80" = {
+          extraConfig = ''
+            root * /srv/http
+            encode zstd gzip
+            file_server
+          '';
+        };
+      };
+
+      services.cloudflared = {
+        enable = true;
+        tunnels."3d321e13-9089-45b4-a862-e6b18b5d22b4" = {
+          credentialsFile = "/home/zexk/.cloudflared/3d321e13-9089-45b4-a862-e6b18b5d22b4.json";
+          default = "http_status:404";
+          ingress = {
+            "zexk.xyz" = "http://localhost:80";
+            "www.zexk.xyz" = "http://localhost:80";
+          };
+        };
+      };
+
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+    };
+}
