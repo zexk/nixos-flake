62a4e2ddcf38c24630ef72c6eae7d4672238636f
Bouraoui Ochi <bouraoui@outlook.it>
2026-06-08 08:25:29 +0200

    vesktop: restructure vencord config with new settings

--- modules/vesktop.nix
+++ modules/vesktop.nix
@@ -1,20 +1,40 @@
 _: {
-  flake.homeModules.vesktop = { ... }: {
-    programs.vesktop = {
-			enable = true;
-			settings = {
-				autoUpdate = false;
-  			autoUpdateNotification = false;
-  			disableMinSize = true;
-  			notifyAboutUpdates = false;
-  			plugins = {
-    			FakeNitro = {
-      			enabled = true;
-    			};
-  			};
-  			useQuickCss = true;
-				enabledThemes = [ "umbra.css" ];
-			};
-		};
-  };
+  flake.homeModules.vesktop =
+    { ... }:
+    {
+      programs.vesktop = {
+        enable = true;
+        settings = {
+          appBadge = false;
+          arRPC = true;
+          checkUpdates = false;
+          customTitleBar = false;
+          disableMinSize = true;
+          minimizeToTray = false;
+          tray = false;
+          splashBackground = "#000000";
+          splashColor = "#ffffff";
+          splashTheming = true;
+          staticTitle = true;
+          hardwareAcceleration = true;
+          discordBranch = "stable";
+        };
+        vencord = {
+          useSystem = true;
+          settings = {
+            autoUpdate = false;
+            autoUpdateNotification = false;
+            disableMinSize = true;
+            notifyAboutUpdates = false;
+            plugins = {
+              FakeNitro = {
+                enabled = true;
+              };
+            };
+            useQuickCss = true;
+            enabledThemes = [ "umbra.css" ];
+          };
+        };
+      };
+    };
 }
