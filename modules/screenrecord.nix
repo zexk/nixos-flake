f9fecc290b6b445666ca32030449f35a2ef46607
Bouraoui Ochi <bouraoui@outlook.it>
2026-06-12 06:43:28 +0200

    screenrecord: use ffmpeg-full, default ffmpeg lacks x11grab

--- modules/screenrecord.nix
+++ modules/screenrecord.nix
@@ -16,7 +16,7 @@ _: {
             read -r x y w h < <(${pkgs.slop}/bin/slop -f '%x %y %w %h') || exit 1
             # libx264 needs even dimensions
             w=$((w / 2 * 2)) h=$((h / 2 * 2))
-            ${pkgs.ffmpeg}/bin/ffmpeg -y -f x11grab -framerate 60 -video_size "''${w}x''${h}" \
+            ${pkgs.ffmpeg-full}/bin/ffmpeg -y -f x11grab -framerate 60 -video_size "''${w}x''${h}" \
               -i "$DISPLAY+$x,$y" "$dir/$(date +%Y-%m-%d-%H%M%S).mp4" &
             echo $! > "$pidfile"
             ${pkgs.libnotify}/bin/notify-send -a clip "recording started"
