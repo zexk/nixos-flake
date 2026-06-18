3dff22b0112c2128bfc6988f9361186eec543ece
Bouraoui Ochi <bouraouiochi@gmail.com>
2026-06-18 21:40:00 +0200

    lightdm: use pkgs.xrandr instead of pkgs.xorg.xrandr

--- modules/lightdm.nix
+++ modules/lightdm.nix
@@ -5,7 +5,7 @@ _: {
       services.xserver.displayManager.lightdm = {
         enable = true;
         extraSeatDefaults = ''
-          display-setup-script = ${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --primary
+          display-setup-script = ${pkgs.xrandr}/bin/xrandr --output DisplayPort-0 --primary
         '';
         greeters.gtk = {
           enable = true;
