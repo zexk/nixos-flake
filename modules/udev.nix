321a5f3a729e6c72a7ed023a30f4361af016b37a
zexk <bouraouiochi@gmail.com>
2026-07-16 21:08:42 +0200

    udev: ignore DualSense auxiliary devices

--- modules/udev.nix
+++ modules/udev.nix
@@ -2,7 +2,10 @@
 {
   flake.nixosModules.udev = _: {
     services.udev.extraRules = ''
-      	ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{product}=="DualSense Wireless Controller", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+      	ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{name}=="Sony Interactive Entertainment Wireless Controller", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+      	ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+      	ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Motion Sensors", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+      	ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Headset Jack", ENV{LIBINPUT_IGNORE_DEVICE}="1"
     '';
   };
 }
